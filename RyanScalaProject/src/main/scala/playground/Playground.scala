d5b17a8c7dcb28afd1c9273a06d9a098bbb75952
RyanColorado04 <ryyo1332@colorado.edu>
2023-02-25 19:11:32 -0600

    Making a second test

--- RyanScalaProject/src/main/scala/playground/Playground.scala
+++ RyanScalaProject/src/main/scala/playground/Playground.scala
@@ -4,5 +4,6 @@ object Playground {
   def main(args: Array[String]): Unit = {
     println("This is the first string in my project")
     println("Adding a second line")
+    // This is a comment
   }
 }
